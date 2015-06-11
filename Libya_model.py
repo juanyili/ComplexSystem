cef5f0e3ad43c2071f133fa9ccbe5adc445ee319
juanyili <wanyili.wendy@gmail.com>
2015-06-11 18:05:02 -0400

    Update Libya_model.py
    Added run_model method and can write 2d array into a .csv file and generate a colored grid.

--- Libya_model.py
+++ Libya_model.py
@@ -17,30 +17,26 @@ class City:
 		self.connections=connections
 		x = 0
 		for c in self.connections:
-			x+=c.unification*np.absolute(np.exp(complex(0,-c.power()*100000000/self.power())))
+			#x+=c.unification*np.absolute(np.exp(complex(0,-c.power()*100000000/self.power())))
+			x+=c.unification*(c.power()/self.power())
 		return x
 
 
 #create cities:
 cities={}
-cities[0]=City('Misu', 9, 12, 9, -1)
-cities[1]=City('Tripoli', 12, 9, 12, 1)
-cities[2]=City('Naf', 3, 9, 6, -1)
+cities[0]=City('Misu', 9, 12, 9, 1)
+cities[1]=City('Tripoli', 12, 9, 12, -1)
+cities[2]=City('Naf', 3, 9, 6, 1)
 cities[3]=City('Zawi', 6, 3, 3, 1)
-cities[4]=City('Zintan', 2, 6, 2, -1)
+cities[4]=City('Zintan', 2, 6, 2, 1)
 
 cities[5]=City('Bengha', 8, 8, 8, -1)
 cities[6]=City('Tubr', 6, 4, 6, 1)
-cities[7]=City('Bayda', 4, 2, 6, -1)
+cities[7]=City('Bayda', 4, 2, 6, 1)
 
 cities[8]=City('Murzu', 1, 2, 1, -1)
-cities[9]=City('Uba', 1, 2, 2, 1)
+cities[9]=City('Uba', 1, 2, 2, -1)
 cities[10]=City('Sab', 2, 1, 3, -1)
-
-#ISIS
-cities[11]=City('ISIS',16,24,24,1)
-
-
 
 #control group
 G=nx.Graph()
@@ -49,36 +45,25 @@ G.add_edges_from([(0,1),(0,2),(0,3),(0,4),(1,2),(1,3),(1,4),(2,3),(2,4),(3,4)])
 G.add_edges_from([(5,6),(5,7),(6,7),(4,5),(4,6),(4,7)])
 G.add_edges_from([(5,6),(6,8),(7,8),(8,10),(9,10)])
 
-dynamicsnoisis=[]
-initial = [cities[i].unification for i in range (11)]
-dynamicsnoisis.append(initial)
-for t in range(50):
-	uni=[]
-	for i in range(11):
-		Idx =  G.neighbors(i)
-		connections = {cities[i] for i in Idx}
-		x = np.sign(cities[i].influence(connections))
-		cities[i].unification = x
-		#print x
-		#print np.sign(cities[i].unification)
-		#if (x - np.sign(cities[i].unification))!=0:
-		#	cities[i].unification= x
-		uni.append(cities[i].unification)
-	dynamicsnoisis.append(uni)
-	#print "Without ISIS, how does Libya look like"
-	#print "At time %s, unification dynamics is %s." % (t, uni)
 
-ar = np.array(dynamicsnoisis)
-import csv
-fl = open('dynamicsnoisis.csv', 'w')
+#create cities:
+cities1={}
+cities1[0]=City('Misu', 9, 12, 9, 1)
+cities1[1]=City('Tripoli', 12, 9, 12, -1)
+cities1[2]=City('Naf', 3, 9, 6, 1)
+cities1[3]=City('Zawi', 6, 3, 3, 1)
+cities1[4]=City('Zintan', 2, 6, 2, 1)
 
-writer = csv.writer(fl)
-for values in ar:
-    writer.writerow(values)
-fl.close()
+cities1[5]=City('Bengha', 8, 8, 8, -1)
+cities1[6]=City('Tubr', 6, 4, 6, 1)
+cities1[7]=City('Bayda', 4, 2, 6, 1)
 
+cities1[8]=City('Murzu', 1, 2, 1, -1)
+cities1[9]=City('Uba', 1, 2, 2, -1)
+cities1[10]=City('Sab', 2, 1, 3, -1)
 
-
+#ISIS
+cities1[11]=City('ISIS',16,24,24,1)
 #experiment group
 H=nx.Graph()
 H.add_nodes_from([0,1,2,3,4,5,6,7,8,9,10,11])
@@ -87,30 +72,36 @@ H.add_edges_from([(5,6),(5,7),(6,7),(4,5),(4,6),(4,7)])
 H.add_edges_from([(5,6),(6,8),(7,8),(8,10),(9,10)])
 H.add_edges_from([(11,0),(11,2),(11,3),(11,4),(11,5),(11,5),(11,7),(11,8),(11,9),(11,10)])
 
-dynamics=[]
-initial = [cities[i].unification for i in range (11)]
-dynamics.append(initial)
-for t in range(50):
-	uni=[]
-	for i in range(11):
-		Idx =  H.neighbors(i)
-		connections = {cities[i] for i in Idx}
-		x = np.sign(cities[i].influence(connections))
-		cities[i].unification = x
-		#if cities[i].influence(connections) != np.sign(cities[i].unification):
-		#	cities[i].unification= -cities[i].unification
-		uni.append(cities[i].unification)
-	dynamics.append(uni)
+def run_model(G, group, filename, picturename):
+	dynamics=[]
+	initial = [group[i].unification for i in range (11)]
+	dynamics.append(initial)
+	for t in range(10):
+		uni=[]
+		for i in range(11):
+			Idx =  G.neighbors(i)
+			connections = {group[i] for i in Idx}
+			x = np.sign(group[i].influence(connections))
+			if x != 0:
+				group[i].unification = x
+			uni.append(group[i].unification)
+		dynamics.append(uni)
 
-#print "With ISIS how does Libya look like?"
-	#print "At time %s, unification dynamics is %s." % (t, uni)
-#print dynamics
+	ar = np.array(dynamics)
+	import csv
+	fl = open(filename, 'w')
 
-ar = np.array(dynamics)
-import csv
-fl = open('dynamics5.csv', 'w')
+	writer = csv.writer(fl)
+	for values in ar:
+	    writer.writerow(values)
+	fl.close()
 
-writer = csv.writer(fl)
-for values in ar:
-    writer.writerow(values)
-fl.close()
+	plt.pcolor(ar)
+	plt.savefig(picturename)
+
+
+#no isis
+run_model(G, cities,'noisis.csv','2dnoisis.png')
+
+#with isis
+run_model(H, cities1,'withisis.csv','2dwithisis.png')
