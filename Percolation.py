ec05bd9f4b7f9dd056dfc5439f9434ac8c83f827
juanyili <wanyili.wendy@gmail.com>
2015-06-15 22:31:04 -0400

    Create Percolation.py
    Starting point of building a random matrix of two colors

---
+++ Percolation.py
@@ -0,0 +1,20 @@
+from pylab import *
+from scipy.ndimage import measurements
+import matplotlib as plt
+import numpy as np
+ 
+L = 100
+r = rand(L,L)
+p = 0.4
+print r.max()
+print r.min()
+mat = np.zeros((L,L))
+for i in range(L):
+	for j in range(L):
+		if r[i,j] < p * (r.max()-r.min()):
+			mat[i,j] = 1
+		else:
+			mat[i,j] = 0
+
+pcolor(mat)
+show()
