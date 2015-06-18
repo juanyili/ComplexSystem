a2d800593b31e7c383c45ab648bf53d5bddbdb66
juanyili <wanyili.wendy@gmail.com>
2015-06-18 14:42:02 -0400

    keep track of the average of number of steps of successful paths.

--- Percolation3.py
+++ Percolation3.py
@@ -2,13 +2,15 @@ from pylab import *
 from scipy.ndimage import measurements
 import matplotlib.pyplot as plt
 import numpy as np
+import time
 
 sucarray=[]
+steparray=[]
 prob=[]
-L=50
+L=20
 for p in np.arange(0.4,0.98,0.001):
 	prob.append(p)
-
+	totalduration =[]
 	success = 0. # keep track of number of successful paths
 	for i in range(100):
 		#generate a new random matrix
@@ -26,7 +28,9 @@ for p in np.arange(0.4,0.98,0.001):
 		old = 0
 		flag = 0 # while it does not reach the bottom
 		# create an array of number of cells each run has gone through
+		start = 0. #create a starting time
 		while flag == 0 and x<L-1:
+			start+=1#one more step
 			downnot=np.random.choice([0, 1],  p=[0.4,0.6]) #probability of going down
 			if downnot == 1 and mat[x+1,y] == 1: # go down
 				x += 1
@@ -47,8 +51,10 @@ for p in np.arange(0.4,0.98,0.001):
 					else: flag = 1 #terminates
 			if x == L-1:
 				success += 1.
+				totalduration.append(start)
 	sucarray.append(success/100)
-
-plot(prob,sucarray,'bo')
-
+	steparray.append(np.sum(totalduration)/success/L if success != 0. else 0)
+print np.mean(np.trim_zeros(steparray))
+#plot(prob,sucarray,'bo')
+plot(prob, steparray,'bo')
 show()
